2480ba4fe5b0040945a4a4e3c2e6bf1a21b8ae86
Mayur <mayurbehere7432@gmail.com>
2024-04-27 01:00:45 +0530

    final commit

--- client/src/components/hero.jsx
+++ client/src/components/hero.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../app.css";
+import "../App.css";
 import bulb from "../assets/bulb.svg";
 import speaker from "../assets/speaker.svg";
 import refresh from "../assets/refresh.svg";
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../assets/logo.svg";
-import "../app.css";
+import "../App.css";
 
 const navbar = () => {
   return (
